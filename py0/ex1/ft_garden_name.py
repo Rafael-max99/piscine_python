9f3efa99e6bd73cae3a3b9a30384e0b0afb825f2
Rafael Laureano Lamego <74817674+Rafael-max99@users.noreply.github.com>
2026-06-04 20:44:57 +0100

    Add files via upload

---
+++ py0/ex1/ft_garden_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def ft_garden_name():
+    name = input("Enter name: ")
+    print(f"Garden: {name}")
+    print("Status: Growing well!")
+
+ft_garden_name()
---
+++ py0/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+
+    def helper(count: int):
+        if count > days:
+            print("Harvest time!")
+            return
+        else:
+            print(f"Day {count}")
+            helper(count + 1)
+
+    helper(1)
+
+ft_count_harvest_recursive()
